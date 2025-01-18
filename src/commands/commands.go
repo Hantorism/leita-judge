e4845d96a8d5af8751acbd06d98f630257eefc04
zzzang12 <wadevs12@gmail.com>
2025-01-19 04:13:46 +0900

    [Move] 소스, 바이너리 파일 위치 이동

--- src/commands/commands.go
+++ src/commands/commands.go
@@ -7,15 +7,15 @@ type Command struct {
 
 var Commands = map[string]Command{
 	"C": {
-		BuildCmd: []string{"gcc", "-o", "submit", "submit.c"},
-		RunCmd:   []string{"./submit"},
+		BuildCmd: []string{"gcc", "-o", "bin/submit", "submit/submit.c"},
+		RunCmd:   []string{"bin/submit"},
 	},
 	"CPP": {
-		BuildCmd: []string{"g++", "-o", "submit", "submit.cpp"},
-		RunCmd:   []string{"./submit"},
+		BuildCmd: []string{"g++", "-o", "bin/submit", "submit/submit.cpp"},
+		RunCmd:   []string{"bin/submit"},
 	},
 	"Java": {
-		BuildCmd: []string{"javac", "Main.java"},
-		RunCmd:   []string{"java", "Main"},
+		BuildCmd: []string{"javac", "-d", "bin", "submit/Main.java"},
+		RunCmd:   []string{"java", "-cp", "bin", "Main"},
 	},
 }
