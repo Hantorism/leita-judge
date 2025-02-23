bf71497757488d532f61ae995bcbe3a7abb4c486
zzzang12 <wadevs12@gmail.com>
2025-02-23 14:14:05 +0900

    refactor: 파일 구조 변경

--- src/routes/router.go
+++ src/routes/router.go
@@ -2,12 +2,10 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"leita/src/apis/problem"
 )
 
 func RegisterRoutes(app *fiber.App) {
 	api := app.Group("/api")
 
-	problemGroup := api.Group("/problem")
-	problemGroup.Post("/:problemId", problem.JudgeProblem)
+	RegisterProblemRoutes(api)
 }
